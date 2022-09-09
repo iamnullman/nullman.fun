34a4c552e73e3df4382010e369e05330851dfa35
NullMan <73245847+iamnullman@users.noreply.github.com>
2022-09-09 06:25:12 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>NullMan</title>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" /> <script src="/js/warning.js" />
